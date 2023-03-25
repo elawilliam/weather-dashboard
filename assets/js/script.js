b4fd3b8dbfb8ea0ca10e800f931baae3a7a93a0b
Angela Witham <ela.witham@gmail.com>
2023-03-25 11:46:34 -0700

    removed the s from HTTP

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,7 @@ function currentWeather(city) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`)
         .then((response) => response.json())
         .then((data) => {
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKey}`)
+            fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=${APIKey}`)
                 .then((response) => response.json())
                 .then((data) => {
                     //current date//
